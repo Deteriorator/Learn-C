5451c9bdb69e8c9e2eeb26bb5debfcaf8ce1a1eb
Leon <Liangz.org@gmail.com>
2021-04-02 23:22:58 +0800

    feat: study auto extern static

--- helloworld/define.c
+++ helloworld/define.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 
 #define PI 3.14
-#define ADD 2+3        //#define ADD (2+3)
+#define ADD (2+3)        //#define ADD (2+3)
 #define MAX(a, b)   ((a) > (b) ? (a) : (b))
 #define max(a, b)   \
     ({int A=a, B=b;((a) > (b) ? (a) : (b));})   //({typeof(a) A=a, B=b;((a) > (b) ? (a) : (b));})
@@ -28,7 +28,8 @@
 //                    有限制，只能定义32位(32机器)大小的数据类型，如double就不行，
 //                    寄存器中没有地址，所以一个寄存器类型的变量无法打印出地址查看
 //                    或使用。
-//          static: 静态型，自动初始化为0值或空值，并且其变量的值有继承性
+//          static: 静态型，自动初始化为0值或空值，并且其变量的值有继承性，常用于修
+//                  饰变量或函数。
 //          extern: 说明型， 意味着不能改变被说明的变量的值或类型 。
 
 int main()
